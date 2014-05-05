df8182d7042aedbc9fd261dfaf47446fe1742286
Inability <inability@InabilitytekiMacBook-Pro.local>
2014-05-05 18:43:50 +0800

    Initial Commit

---
+++ myzip/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  myzip
+//
+//  Created by Inability on 14-5-5.
+//  Copyright (c) 2014年 Inability. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
